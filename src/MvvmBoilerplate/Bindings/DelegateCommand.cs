1c336f96fa5a3f434c6d45243c5a48293a443320
Tatsuya MIDORIKAWA <midorikawa@any-one.jp>
2020-08-18 10:21:15 +0900

    💡初回コミット

---
+++ src/MvvmBoilerplate/Bindings/DelegateCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MvvmLight
+{
+    public class DelegateCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+            => _canExecute();
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+            => _execute();
+
+        public DelegateCommand(Action execute) : this(execute, () => true) { }
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+    }
+
+    public class DelegateCommand<T> : ICommand
+    {
+        public bool CanExecute(object parameter)
+            => _canExecute(Cast(parameter));
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+            => _execute(Cast(parameter));
+
+        private T Cast(object parameter)
+            => (parameter == null) ? default : (T)parameter;
+
+        public DelegateCommand(Action<T> execute) : this(execute, _ => true) { }
+        public DelegateCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+    }
+}
---
+++ src/MvvmBoilerplate/Bindings/NotifyProperty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace MvvmLight
+{
+    public class NotifyProperty<T> : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (EqualityComparer<T>.Default.Equals(_value, value))
+                    return;
+                _value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+        private T _value;
+
+        public NotifyProperty() : this(default) { }
+        public NotifyProperty(T value)
+            => _value = value;
+    }
+}
